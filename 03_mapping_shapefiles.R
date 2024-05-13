640db7a06623c0c41963fe377a0dbb5f8897042e
erin.mchugh@esc3055m-srv-frederickson.eeb.utoronto.ca <e.mchugh@mail.utoronto.ca>
2024-05-13 09:55:09 -0400

    final changes to mapping shapefiles

--- 03_mapping_shapefiles.R
+++ 03_mapping_shapefiles.R
@@ -30,25 +30,13 @@ for (x in 1:length(legume_pol$polygon)) {
   if(legume_pol$polygon[[x]]@data$Code %in% tmp$code) next()
   tmp <- rbind((data.frame(code = legume_pol$polygon[[x]]@data$Code, index = x)), tmp)
 }
-unique_pols <- legume_pol[tmp$index, ]
-unique_pols <- cbind(unique_pols, tmp)
 
 legume_pol$code<-NA
 for (x in 1:length(legume_pol$polygon)) {
   legume_pol[x, "code"] <- legume_pol$polygon[[x]]@data$Code 
 }
 
-lspecies_counts <- subset(legume_pol, introduced_status == "N") %>%
-  group_by(code) %>%
-  summarise(num_species = n_distinct(species))
-
-# Merge with unique polygons
-merge <- sp::merge(unique_pols, lspecies_counts, by.x="code", by.y="code")
-
-
-spatial_polygons <- do.call(rbind, merge$`polygon`)
-attribute_data <- merge[c("species", "num_species")]
-spatial_polygons_df <- SpatialPolygonsDataFrame(spatial_polygons, data = attribute_data, match.ID = F)
+spatial_polygons_df <- SpatialPolygonsDataFrame(spatial_polygons, data = legume_pol, match.ID = F)
 spatial_polygons_sf <- sf::st_as_sf(spatial_polygons_df)
 
 subset_polygon<-filter(spatial_polygons_sf, species=="Abrus fruticulosus"|species=="Abrus precatorius"|species=="Acacia acinacea")
