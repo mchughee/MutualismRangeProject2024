7b70215547e35a86b3de1e4d59879f2806ebb1ba
erin.mchugh@esc3055m-srv-frederickson.eeb.utoronto.ca <e.mchugh@mail.utoronto.ca>
2025-01-21 15:57:43 -0500

    last commit I promise!!

--- 12_dropping_polytomy.R
+++ 12_dropping_polytomy.R
@@ -8,6 +8,12 @@ library(ggtree)
 # Bring in tree
 mytree<-read.tree("phylogeny_2771_buildnodes1.tre")
 
+p <- ggtree(mytree, alpha=0.1, layout="circular")+geom_nodelab(aes(label = node))
+p <- p %>% ggtree::collapse(node=2775)
+p <- p %>% ggtree::collapse(node=4185)
+p <- p %>% ggtree::collapse(node=2773)
+p <- p + geom_tiplab()
+p
 
 # get a cute tibble of the tree to figure out which species
 # form this giant polytomy
@@ -24,7 +30,7 @@ write.csv(species_to_drop, "list_of_species_in_polytomy_jan2025.csv")
 
 # filter out species with a branch length that is 84.763337
 
-dropped<-drop.tip(mytree, 1:139)
+dropped<-drop.tip(mytree, species_to_drop$label)
 
 # Check that it worked by looking at output as a tibble
 check<-as_tibble(dropped)
@@ -36,3 +42,4 @@ plot(dropped)
 
 # write tree
 write.tree(dropped, "polytomy_removed.tre")
+
