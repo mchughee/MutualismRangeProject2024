d886e1bf3d5c798924871da28258d1390af3dc7c
meganbontrager <mgbontrager@gmail.com>
2026-03-02 11:09:59 -0500

    added reference to temperature raster but now spatsample gives me an error

--- code/03_thin_occs.R
+++ code/03_thin_occs.R
@@ -6,8 +6,13 @@ library(sf)
 library(dplyr)
 
 # Using the twenty species dataframe for right now, but replace with full data when the time comes
-occ<-read.csv("allocc_clean.csv")
+occ<-read.csv("data/allocc_clean.csv")
+
 occ$species<-gsub(" ", "_", occ$species)
+
+# Read in temperature raster to supply cells to sample
+# 30s Bioclim data from here https://geodata.ucdavis.edu/climate/worldclim/2_1/base/wc2.1_30s_bio.zip
+temp<-raster::raster("wc2.1_30s_bio_1.tif")
 
 
 # tell R where the long/lat is in the dataframe and the crs
@@ -19,28 +24,16 @@ occs_ls <- terra::vect(occ, geom = c("decimalLongitude",
 
 # use spatsample (terra) to thin data to one observation per cell BUT per species
 
-
 results<-NULL
-  for(i in (unique(occs_ls$species))){
-    this.species<- spatSample(occs_ls[occs_ls$species==i,], size=1, strata=temp)
-    my_sf<-sf::st_as_sf(this.species)
-    results<-rbind(results, my_sf)
-    print(i)}
 
-    results1<-results %>% dplyr::select(-c(X))
-    sf::st_write(results1, "thinned_data.csv", layer_options = "GEOMETRY=AS_XY")
+for (i in length(unique(occs_ls$species))) {
+  this.species<- spatSample(occs_ls[occs_ls$species==i,], size=1, strata = temp)
+  my_sf<-sf::st_as_sf(this.species)
+  results<-rbind(results, my_sf)
+  print(i)
+    }
 
-
-# Check to ensure that data is actually in the right format:')
-thinned_data<-read.csv("thinned_data.csv")
-
-# Okay great, it's working and read in! Now, make sure it's in sf format
-
-thinned <- st_as_sf(x = thinned_data,                         
-                    coords = c("X", "Y"))
-# Tell R to read coordinates as WGS84
-st_set_crs(thinned, 4326)->thinned
+sf::st_write(results1, "data/thinned_data.csv", layer_options = "GEOMETRY=AS_XY")
 
 
 # Nice and easy! Now we have a thinned dataframe
-
